6d9f3272c378b1504193dcee59c1dcbefb366f59
rohit george <code.mastermind.07@gmail.com>
2023-09-30 23:24:32 +0530

    added the start screen adding levels soon

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-# Import necessary packages
 import cv2
 import numpy as np
 import mediapipe as mp
@@ -12,11 +11,41 @@ import time
 pygame.init()
 
 # Set up the display in full-screen mode
-screen = pygame.display.set_mode((1080,1920), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((1080, 1920), pygame.FULLSCREEN)
 pygame.display.set_caption("Hand Gesture Game")
 # Get the dimensions of the screen
 screen_width, screen_height = pygame.display.get_surface().get_size()
-fullscreen=True
+fullscreen = True
+
+# Create a variable to track the game state
+game_started = False
+
+# Load a background image for the start screen
+start_screen_bg = pygame.image.load("start_screen_bg.jpg")
+start_screen_bg = pygame.transform.scale(start_screen_bg, (screen_width, screen_height))
+
+# Function to display the start screen
+def display_start_screen():
+    # Display the background image
+    screen.blit(start_screen_bg, (0, 0))
+
+    # Style the "Press Enter to Start" text
+    start_font = pygame.font.Font(None, 72)
+    text_color = (255, 255, 255)
+    text_shadow_color = (0, 0, 0)
+    start_text = start_font.render("Press Enter to Start", True, text_color)
+    
+    # Create a shadow for the text
+    shadow_offset = 4
+    shadow_text = start_font.render("Press Enter to Start", True, text_shadow_color)
+    
+    # Display the shadow text
+    screen.blit(shadow_text, (screen_width // 2 - shadow_text.get_width() // 2 + shadow_offset,
+                              screen_height // 2 - shadow_text.get_height() // 2 + shadow_offset))
+    
+    # Display the actual text
+    screen.blit(start_text, (screen_width // 2 - start_text.get_width() // 2,
+                            screen_height // 2 - start_text.get_height() // 2))
 
 # Load available gestures and their corresponding images
 gesture_images = {
@@ -26,11 +55,10 @@ gesture_images = {
     "okay": pygame.image.load("ok.jpg"),
     "peace": pygame.image.load("peace.jpg"),
     "thumbs up": pygame.image.load("thumbs up.jpg"),
-   
+
     "stop": pygame.image.load("stop.jpg"),
     "smile": pygame.image.load("smile.jpg"),
     "live long": pygame.image.load("live long.jpg"),
-    
 }
 
 # Create a list of available gesture names
@@ -45,22 +73,21 @@ feedback_duration = 3  # Increase the feedback duration to 3 seconds
 feedback_start_time = 0  # Timestamp when feedback started
 game_over = False  # Game over state
 
-
 # Initialize feedback variables
 feedback_color = (0, 0, 0)  # Default feedback color (black)
 feedback_text = ""
 
 # Initialize mediapipe
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
-mpDraw = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mp_draw = mp.solutions.drawing_utils
 
 # Load the gesture recognizer model
 model = load_model('mp_hand_gesture')
 
 # Load class names
 with open('gesture.names', 'r') as f:
-    classNames = f.read().split('\n')
+    class_names = f.read().split('\n')
 
 # Initialize the webcam
 cap = cv2.VideoCapture(0)
@@ -69,9 +96,8 @@ cap = cv2.VideoCapture(0)
 fullscreen = False
 # Timer variables
 countdown_start_time = 0
-countdown_duration = 60  # 1 minute
+countdown_duration = 10 #10 sec
 countdown_color = (0, 0, 255)  # Default color (blue)
-
 
 while True:
     for event in pygame.event.get():
@@ -83,184 +109,188 @@ while True:
 
         # Check for keyboard events
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:  # Check if the Esc key is pressed
+            if event.key == pygame.K_ESCAPE:
                 # Exit full-screen mode
-                pygame.display.set_mode((800, 600))  # Set the display to a specific size (windowed mode)
-                screen = pygame.display.set_mode((800, 600))  # Update the screen variable
-                pygame.display.set_caption("Hand Gesture Game")  # Restore the window title
+                pygame.display.set_mode((800, 600))
+                screen = pygame.display.set_mode((800, 600))
+                pygame.display.set_caption("Hand Gesture Game")
                 fullscreen = False
             elif event.key == pygame.K_f:
                 if fullscreen:
                     # Exit full-screen mode
-                    pygame.display.set_mode((800, 600))  # Set the display to a specific size (windowed mode)
-                    screen = pygame.display.set_mode((800, 600))  # Update the screen variable
-                    pygame.display.set_caption("Hand Gesture Game")  # Restore the window title
+                    pygame.display.set_mode((800, 600))
+                    screen = pygame.display.set_mode((800, 600))
+                    pygame.display.set_caption("Hand Gesture Game")
                 else:
                     # Enter full-screen mode
-                    screen = pygame.display.set_mode((800,600))
+                    screen = pygame.display.set_mode((800, 600))
                     fullscreen = True
+            elif event.key == pygame.K_RETURN:
+                if not game_started:
+                    game_started = True
 
+    # Display the start screen if the game has not started
+    if not game_started:
+        display_start_screen()
+    else:
+        # The rest of your game code here...
+        if not game_over:
+            if fullscreen == False:
+                # Read each frame from the webcam
+                _, frame = cap.read()
 
-    if not game_over:
-        if fullscreen==False:
-        # Read each frame from the webcam
-            _, frame = cap.read()
+                # Flip the frame vertically
+                frame = cv2.flip(frame, 1)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Flip the frame vertically
-            frame = cv2.flip(frame, 1)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # Get hand landmark prediction
+                result = hands.process(frame)
 
-        # Get hand landmark prediction
-            result = hands.process(frame)
+                className = ''
 
-            className = ''
+                frame = cv2.resize(frame, (screen_width, screen_height))
 
-            frame = cv2.resize(frame, (screen_width,screen_height))
+                x, y, _ = frame.shape
+            else:
+                # Read each frame from the webcam
+                _, frame = cap.read()
 
-            x, y, _ = frame.shape
-        else:
-            # Read each frame from the webcam
-            _, frame = cap.read()
+                # Flip the frame vertically
+                frame = cv2.flip(frame, 1)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Flip the frame vertically
-            frame = cv2.flip(frame, 1)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # Get hand landmark prediction
+                result = hands.process(frame)
 
-        # Get hand landmark prediction
-            result = hands.process(frame)
+                className = ''
 
-            className = ''
+                frame = cv2.resize(frame, (800, 600))
 
-            frame = cv2.resize(frame, (800,600))
+                x, y, _ = frame.shape
 
-            x, y, _ = frame.shape
+            # Post-process the result
+            if result.multi_hand_landmarks:
+                current_time = time.time()
+                elapsed_time = current_time - gesture_read_time
+                landmarks = []
+                for handslms in result.multi_hand_landmarks:
+                    for lm in handslms.landmark:
+                        lmx = int(lm.x * x)
+                        lmy = int(lm.y * y)
+                        landmarks.append([lmx, lmy])
+                    # Drawing landmarks on frames
+                    mp_draw.draw_landmarks(frame, handslms, mp_hands.HAND_CONNECTIONS)
+                    # Predict gesture
+                    prediction = model.predict([landmarks], verbose=0)
+                    classID = np.argmax(prediction)
+                    className = class_names[classID]
+                    gesture_read_time = current_time
 
+            # disply video
+            surface = pygame.surfarray.make_surface(frame)
+            surface = pygame.transform.rotate(surface, -90)
+            screen.blit(surface, (0, 0))
 
-        # Post-process the result
-        if result.multi_hand_landmarks:
+            # Show the prediction on the frame
+            pred_font = pygame.font.Font(None, 48)
+            pred_font_render = pred_font.render(className, True, (255, 255, 255))
+            screen.blit(pred_font_render, (20, 600 - pred_font_render.get_height() - 20))
+
+            # Get the current target gesture
+            current_target = available_gestures[score % len(available_gestures)]
+            target_image = gesture_images[current_target]
+            target_image = pygame.transform.scale(target_image, (300, 300))
+            screen.blit(target_image, (0, 0))
+
+            # Show feedback animation
             current_time = time.time()
-            elapsed_time = current_time - gesture_read_time
-            landmarks = []
-            for handslms in result.multi_hand_landmarks:
-                for lm in handslms.landmark:
-                    lmx = int(lm.x * x)
-                    lmy = int(lm.y * y)
-                    landmarks.append([lmx, lmy])
-                # Drawing landmarks on frames
-                mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
-                # Predict gesture
-                prediction = model.predict([landmarks], verbose=0)
-                classID = np.argmax(prediction)
-                className = classNames[classID]
-                gesture_read_time = current_time
+            feedback_elapsed_time = current_time - feedback_start_time
+            if feedback_elapsed_time <= feedback_duration:
+                feedback_alpha = int(255 - (feedback_elapsed_time / feedback_duration) * 255)
+                feedback_surface = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
+                feedback_surface.fill((feedback_color[0], feedback_color[1], feedback_color[2], feedback_alpha))
+                screen.blit(feedback_surface, (0, 0))
+                font = pygame.font.Font(None, 48)
+                feedback_text_render = font.render(feedback_text, True, (255, 255, 255))
+                screen.blit(feedback_text_render, (400 - feedback_text_render.get_width() // 2, 300))
 
-        # disply video
-        surface = pygame.surfarray.make_surface(frame)
-        surface = pygame.transform.rotate(surface, -90)
-        screen.blit(surface, (0, 0))
+            # Check if the recognized gesture matches the target
+            if className == current_target:
+                if className not in user_gestures:
+                    user_gestures.append(className)
+                    score += 1
+                    feedback_color = (0, 250, 0)  # Green for correct gesture
+                    feedback_text = "Correct!"
+                    feedback_start_time = current_time  # Reset feedback timer for correct gesture
+                    pygame.draw.rect(screen, feedback_color, (0, 0, screen_width, screen_height))
 
-        # Show the prediction on the frame
-        pred_font = pygame.font.Font(None, 48)
-        pred_font_render = pred_font.render(className, True, (255, 255, 255))
-        screen.blit(pred_font_render, (20, 600 - pred_font_render.get_height() - 20))
+                    # Draw the "Correct!" text
+                    font = pygame.font.Font(None, 100)
+                    text_render = font.render(feedback_text, True, (255, 255, 255))
+                    text_rect = text_render.get_rect(center=(650, 300))
+                    screen.blit(text_render, text_rect.center)
 
-        # Get the current target gesture
-        current_target = available_gestures[score % len(available_gestures)]
-        target_image = gesture_images[current_target]
-        target_image = pygame.transform.scale(target_image, (300, 300))
-        screen.blit(target_image, (0, 0))
+                    pygame.display.flip()  # Update the display
 
-      
+                    # Wait for 3 seconds
+                    time.sleep(3)
+                    feedback_color = (0, 0, 0)
+                    feedback_text = ""
 
-        # Show feedback animation
-        current_time = time.time()
-        feedback_elapsed_time = current_time - feedback_start_time
-        if feedback_elapsed_time <= feedback_duration:
-            feedback_alpha = int(255 - (feedback_elapsed_time / feedback_duration) * 255)
-            feedback_surface = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
-            feedback_surface.fill((feedback_color[0], feedback_color[1], feedback_color[2], feedback_alpha))
-            screen.blit(feedback_surface, (0, 0))
-            font = pygame.font.Font(None, 48)
-            feedback_text_render = font.render(feedback_text, True, (255, 255, 255))
-            screen.blit(feedback_text_render, (400 - feedback_text_render.get_width() // 2, 300))
+                    # Start the countdown timer
+                    countdown_start_time = current_time
+                    countdown_color = (0, 0, 255)  # Reset countdown color to blue
 
-        # Check if the recognized gesture matches the target
-        if className == current_target:
-            if className not in user_gestures:
-                user_gestures.append(className)
-                score += 1
-                feedback_color = (0, 250, 0)  # Green for correct gesture
-                feedback_text = "Correct!"
-                feedback_start_time = current_time  # Reset feedback timer for correct gesture
-                pygame.draw.rect(screen, feedback_color, (0, 0, screen_width, screen_height))
-
-                # Draw the "Correct!" text
-                font = pygame.font.Font(None, 100)
-                text_render = font.render(feedback_text, True, (255, 255, 255))
-                text_rect = text_render.get_rect(center=(650, 300))
-                screen.blit(text_render, text_rect.center)
-
-                pygame.display.flip()  # Update the display
-
-                # Wait for 3 seconds
-                time.sleep(3) 
-                feedback_color = (0, 0, 0)
+            else:
+                feedback_color = (0, 0, 0)  # Reset feedback color to default (black)
                 feedback_text = ""
 
-                # Start the countdown timer
-                countdown_start_time = current_time
-                countdown_color = (0, 0, 255)  # Reset countdown color to blue
-           
+            # Calculate the remaining time for the countdown timer
+            countdown_elapsed_time = current_time - countdown_start_time
+            remaining_time = max(0, countdown_duration - countdown_elapsed_time)
+
+            # Change countdown color to red after 50 seconds
+            if remaining_time <= 10:
+                countdown_color = (255, 0, 0)  # Red color
+
+            # Display the countdown timer
+            font = pygame.font.Font(None, 72)
+            countdown_text = font.render(f"Time Left: {int(remaining_time)}", True, countdown_color)
+            screen.blit(countdown_text, (screen_width // 2 - countdown_text.get_width() // 2, 20))
+
+            # Check if the countdown timer has ended
+            if remaining_time <= 0:
+                feedback_start_time = 0  # Reset feedback timer
+                user_gestures = []  # Clear user gestures
+                current_target = random.choice(available_gestures)  # Choose a new random target gesture
+                countdown_start_time = current_time  # Restart the countdown timer
+
+        # Game Over screen and Restart button
         else:
-            feedback_color = (0, 0, 0)  # Reset feedback color to default (black)
-            feedback_text = ""
+            # Display "Game Over" message
+            game_over_font = pygame.font.Font(None, 92)
+            game_over_text = game_over_font.render("Game Over", True, (255, 0, 0))
+            screen.blit(game_over_text, (screen_width // 2 - game_over_text.get_width() // 2, 300))
 
-    # Calculate the remaining time for the countdown timer
-        countdown_elapsed_time = current_time - countdown_start_time
-        remaining_time = max(0, countdown_duration - countdown_elapsed_time)
+            # Display "Restart" button
+            restart_prompt_font = pygame.font.Font(None, 48)
+            restart_prompt_text = restart_prompt_font.render("Press Enter to Restart", True, (0, 255, 0))
+            screen.blit(restart_prompt_text, (screen_width // 2 - restart_prompt_text.get_width() // 2, 350))
 
-        # Change countdown color to red after 50 seconds
-        if remaining_time <= 10:
-            countdown_color = (255, 0, 0)  # Red color
+            # Handle keyboard input to restart the game
+            if game_over:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_RETURN]:  # Check if the Enter key is pressed
+                    # Reset game variables
+                    score = 0
+                    user_gestures = []
+                    game_over = False
+                    restart_pending = False
 
-        # Display the countdown timer
-        font = pygame.font.Font(None, 72)
-        countdown_text = font.render(f"Time Left: {int(remaining_time)}", True, countdown_color)
-        screen.blit(countdown_text, (screen_width // 2 - countdown_text.get_width() // 2, 20))
-
-        # Check if the countdown timer has ended
-        if remaining_time <= 0:
-            feedback_start_time = 0  # Reset feedback timer
-            user_gestures = []  # Clear user gestures
-            current_target = random.choice(available_gestures)  # Choose a new random target gesture
-            countdown_start_time = current_time  # Restart the countdown timer
-
-    # Game Over screen and Restart button
-    else:
-        # Display "Game Over" message
-        game_over_font = pygame.font.Font(None, 92)
-        game_over_text = game_over_font.render("Game Over", True, (255, 0, 0))
-        screen.blit(game_over_text, (screen_width // 2 - game_over_text.get_width() // 2, 300))
-
-        # Display "Restart" button
-        restart_prompt_font = pygame.font.Font(None, 48)
-        restart_prompt_text = restart_prompt_font.render("Press Enter to Restart", True, (0, 255, 0))
-        screen.blit(restart_prompt_text, (screen_width // 2 - restart_prompt_text.get_width() // 2, 350))
-
-        # Handle keyboard input to restart the game
-        if game_over:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_RETURN]:  # Check if the Enter key is pressed
-                # Reset game variables
-                score = 0
-                user_gestures = []
-                game_over = False
-                restart_pending = False
-
-    # Draw score on the screen
-    font = pygame.font.Font(None, 36)
-    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
-    screen.blit(score_text, (screen_width - score_text.get_width() - 10, 0))
+        # Draw score on the screen
+        font = pygame.font.Font(None, 36)
+        score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+        screen.blit(score_text, (screen_width - score_text.get_width() - 10, 0))
 
     # Update the Pygame display
     pygame.display.update()
